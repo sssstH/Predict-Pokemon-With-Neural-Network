ca73a167f4b2cff5a9397deb0ec5398bb4533759
Haoshen <haoshen0226@gmail.com>
2024-06-08 20:29:28 +0800

    update

---
+++ ModelTraining.py
@@ -0,0 +1,41 @@
+import numpy as np
+import nnfs
+from ModelClasses import *
+from sklearn.model_selection import train_test_split
+
+nnfs.init()
+
+labels = os.listdir('PokemonData')
+# X, y= create_data_mnist('PokemonData')
+X, y = input_target_split('PokemonData', labels)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=42)
+X_train_reshaped = (X_train.reshape(X_train.shape[0], -1).astype(np.float32) - 127.5) / 127.5
+X_test_reshaped = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5) / 127.5
+
+# Instantiate the model
+model = Model()
+
+# Add layers
+model.add(Layer_Dense(X_train_reshaped.shape[1], 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 150))
+model.add(Activation_Softmax())
+
+# Set loss, optimizer and accuracy objects
+model.set(
+    loss=Loss_CategoricalCrossentropy(),
+    optimizer=Optimizer_Adam(decay=1e-3),
+    accuracy=Accuracy_Categorical()
+)
+
+# Finalize the model
+model.finalize()
+
+# Train the model
+model.train(X_train_reshaped, y_train, validation_data=(X_test_reshaped, y_test),
+            epochs=10, batch_size=128, print_every=100)
+
+model.save('pokemon.model')
---
+++ README.md
@@ -0,0 +1,24 @@
+# Pokemon-Image-Predictor
+This project uses **Sequential Feedforward Neural Network** to Predict **Pokemon** with images.
+
+## Prediction Result
+
+Model Training Accuracy: **0.014**  
+Validation Accuracy: **0.008**  
+
+![Prediction Image](/assets/Prediction.png)
+
+## Setup the environment
+
+```bash
+pip install -r requirements.txt
+```
+
+## Thoughts & Future
+
+1. The prediction is FUNNY :D
+2. The simple feedforward NN might perform bad on complex image classification. I will try CNN or other NN structure in the future to complete this project.
+
+## References
+
+1. Ideas for processing the data: https://www.kaggle.com/code/quadeer15sh/classification-and-pokemon-api-90-f1-score
